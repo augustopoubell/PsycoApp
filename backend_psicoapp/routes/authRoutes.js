b8f1784a7b0401e24a01a517b48440ab03694173
Fernandaaaa <62066069+Fernandaaaa@users.noreply.github.com>
2025-05-30 15:57:09 -0300

    authRoutes.js

--- backend_psicoapp/routes/authRoutes.js
+++ backend_psicoapp/routes/authRoutes.js
@@ -5,4 +5,5 @@ const authController = require('../controllers/authController');
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
 router.get('/users/:id', authController.getUserById);
+
 module.exports = router;
